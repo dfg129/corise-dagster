9de540d6677c90111848f45f2cd36d275072faf8
David Gibbons <dgibbons@LM-DGIBBONS.local>
2022-09-11 14:04:12 -0400

    first pass

--- week_4/project/week_4.py
+++ week_4/project/week_4.py
@@ -1,26 +1,61 @@
 from typing import List
 
-from dagster import Nothing, asset, with_resources
+from project.resources import S3, Redis, redis_resource, s3_resource
+
+from dagster import Nothing, asset, Out, repository, with_resources
 from project.resources import redis_resource, s3_resource
 from project.types import Aggregation, Stock
+from datetime import datetime
 
 
-@asset
-def get_s3_data():
-    # Use your op logic from week 3
-    pass
+@asset(
+     required_resource_keys={"s3"},
+)
+def get_s3_data(context, group_name="corise"):
+    output = list()
+    stocks = context.resources.s3.get_data("key_name")
+    for row in stocks:
+        stock = Stock.from_list(row)
+        output.append(stock)
+    return output
 
 
-@asset
-def process_data():
-    # Use your op logic from week 3 (you will need to make a slight change)
-    pass
+@asset()
+def process_data(get_s3_data):
+    high_val = 0
+    date = datetime
+    
+    for stock in get_s3_data:
+        if stock.high > high_val:
+            high_val = stock.high
+            date = stock.date
+    
+    return Aggregation(date=date, high=high_val)
 
+@asset(
+    required_resource_keys={"redis"},
+)
+def put_redis_data(context, process_data):
+   context.resources.redis.put_data(process_data.date, process_data.high)
+   
 
-@asset
-def put_redis_data():
-    # Use your op logic from week 3 (you will need to make a slight change)
-    pass
-
-
-get_s3_data_docker, process_data_docker, put_redis_data_docker = with_resources()
+get_s3_data_docker, process_data_docker, put_redis_data_docker = with_resources(
+    definitions=[get_s3_data, process_data, put_redis_data],
+    resource_defs={"redis": redis_resource, "s3": s3_resource},
+    resource_config_by_key={
+        "s3": {
+            "config": {
+                    "bucket": "dagster",
+                    "access_key": "test",
+                    "secret_key": "test",
+                    "endpoint_url": "http://host.docker.internal:4566"
+            }
+        },
+        "redis": {
+            "config": {
+                "host": 'redis',
+                "port": 6379,
+            }
+        }
+    }
+)
