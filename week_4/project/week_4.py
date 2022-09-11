7d5494280cc44da7dac2b1e1e38fdd3f42f818d3
David Gibbons <dgibbons@LM-DGIBBONS.local>
2022-09-11 18:33:31 -0400

    pass with one fail

--- week_4/project/week_4.py
+++ week_4/project/week_4.py
@@ -1,8 +1,7 @@
 from typing import List
 
-from project.resources import S3, Redis, redis_resource, s3_resource
 
-from dagster import Nothing, asset, Out, repository, with_resources
+from dagster import Nothing, op, asset, Out, repository, with_resources
 from project.resources import redis_resource, s3_resource
 from project.types import Aggregation, Stock
 from datetime import datetime
@@ -10,8 +9,9 @@ from datetime import datetime
 
 @asset(
      required_resource_keys={"s3"},
+     group_name="corise"
 )
-def get_s3_data(context, group_name="corise"):
+def get_s3_data(context):
     output = list()
     stocks = context.resources.s3.get_data("key_name")
     for row in stocks:
@@ -20,7 +20,9 @@ def get_s3_data(context, group_name="corise"):
     return output
 
 
-@asset()
+@asset(
+    group_name="corise"
+)
 def process_data(get_s3_data):
     high_val = 0
     date = datetime
@@ -34,6 +36,7 @@ def process_data(get_s3_data):
 
 @asset(
     required_resource_keys={"redis"},
+    group_name="corise"
 )
 def put_redis_data(context, process_data):
    context.resources.redis.put_data(process_data.date, process_data.high)
@@ -43,6 +46,11 @@ get_s3_data_docker, process_data_docker, put_redis_data_docker = with_resources(
     definitions=[get_s3_data, process_data, put_redis_data],
     resource_defs={"redis": redis_resource, "s3": s3_resource},
     resource_config_by_key={
+        # "key_name": {
+        #     "config": {
+        #         "s3_key": "prefix/stock.csv"
+        #     }
+        # },
         "s3": {
             "config": {
                     "bucket": "dagster",
@@ -57,5 +65,6 @@ get_s3_data_docker, process_data_docker, put_redis_data_docker = with_resources(
                 "port": 6379,
             }
         }
-    }
+    },
 )
+
